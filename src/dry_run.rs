0c219157abb10f0f06fba242dbbfc4c6c01b54ef
Jack Bennett <jackbennett0708@gmail.com>
2024-05-25 16:51:15 +0100

    Add pipe prefix to template dry-runs and better colour formatting
    In project templates, output file names are now bold whilst the
    leading folder prefix is darker.

--- src/dry_run.rs
+++ src/dry_run.rs
@@ -7,23 +7,43 @@
 
 use std::collections::HashMap;
 
-use colored::Colorize;
+use colored::{ColoredString, Colorize};
 
-const INDENT_PREFIX: &'static str = "    ";
+const INDENT_PREFIX: &'static str = "   | ";
 
 pub fn print_file_render<S: AsRef<str>>(name: S, render: S) {
-    println!(
-        "{}:\n{}{}",
-        format!("\"{}\"", name.as_ref()).green(),
-        &INDENT_PREFIX,
-        render
-            .as_ref()
-            .replace("\n", format!("\n{}", &INDENT_PREFIX).as_str())
+    file_output(
+        format!(
+            "{}{}{}",
+            "[".green(),
+            name.as_ref().green().bold(),
+            "]".green()
+        )
+        .into(),
+        render.as_ref().to_string(),
     );
 }
 
 pub fn print_project_render<S: AsRef<str>>(outputs: HashMap<S, S>) {
-    for (output, render) in outputs {
-        print_file_render(&output, &render);
+    for (output, render) in &outputs {
+        let (prefix, fname) = if let Some(pos) = output.as_ref().rfind("/") {
+            (&output.as_ref()[..(pos + 1)], &output.as_ref()[(pos + 1)..])
+        } else {
+            ("", output.as_ref())
+        };
+
+        file_output(
+            format!("{}{}", prefix.green(), fname.green().bold()).into(),
+            render.as_ref().to_string(),
+        );
     }
+}
+
+fn file_output(head: ColoredString, text: String) {
+    println!(
+        "{}:\n{}{}",
+        &head,
+        &INDENT_PREFIX.green(),
+        &text.replace("\n", format!("\n{}", &INDENT_PREFIX.green()).as_str())
+    );
 }
